3552a8868a6b450f26a3824ae5ad78d59fd90f18
Jack Lambert <jackwlambert14@gmail.com>
2023-02-15 02:14:00 -0500

    first commit

---
+++ PlusOne.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+        for(int count = digits.Length; count >= 0; count--) {
+
+        }
+        int[] digits2 = new int[digits.Length+1];
+        digits2[0] = 
+    }
+    }
