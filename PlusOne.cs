8bc78969f9950c7dd172bb2fb948ecb34132c734
Jack Lambert <jackwlambert14@gmail.com>
2023-02-16 14:36:03 -0500

    solve with for loop

--- PlusOne.cs
+++ PlusOne.cs
@@ -13,4 +13,25 @@ public class Solution {
         digits[count]++;
         return digits;
     }
+    public int[] PlusOneV2(int[] digits) {
+        int count = digits.Length -1;
+        for(count = digits.Length -1; count >= 0; count--) {
+            if(count >= 0 && digits[count] == 9) {
+                digits[count] = 0;
+            } else {
+            digits[count]++;
+            break;
+            }
+        }
+            if( count < 0) {
+                Array.Resize(ref digits, digits.Length+1);
+                count = 0;
+                digits[count]++;
+            }
+            foreach(int i in digits) {
+                Console.Write($" {i} ");
+            }
+            
+            return digits;
+    }
     }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Solution sol = new();
+int[] numbies = new[] {9};
+sol.PlusOneV2(numbies);
